baa33d0a9f256cc2080ff0d0ef47cd9269c40399
Maryna <maryna.lysyak@gmail.com>
2023-02-06 13:35:18 +0300

    Задача с координатами. Ввод переменных через функцию

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+
+// Функция, с помощью которой можно сократить строки описания ввода переменных.
+
+int InputNumber(string stroka)
+{
+    System.Console.Write(stroka);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int x = InputNumber("Введите координату х: ");
+int y = InputNumber("Введите координату у: ");
+
+// System.Console.Write("Введите координату X: "); 
+// int x = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.Write("Введите координату Y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    System.Console.WriteLine("Точка с такими координатами попадает в 1 четверть");
+}
+else if (x > 0 && y < 0)
+{
+    System.Console.WriteLine("Точка с такими координатами попадает во 2 четверть");
+}
+else if (x < 0 && y > 0)
+{
+    System.Console.WriteLine("Точка с такими координатами попадает в 4 четверть");
+}
+else if (x < 0 && y < 0)
+{
+    System.Console.WriteLine("Точка с такими координатами попадает в 3 четверть");
+}
+else
+{
+    System.Console.WriteLine("Точка соответствует началу координат");
+}
