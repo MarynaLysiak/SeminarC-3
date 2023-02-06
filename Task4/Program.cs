2fe44be5b01d42592bb0107005e4195f7466a440
Maryna <maryna.lysyak@gmail.com>
2023-02-06 16:50:27 +0300

    Решение задачи через For

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -7,11 +7,17 @@
 System.Console.WriteLine("Введите число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
 
+// int count = 1;
 
-int count = 1;
+// while (count <= N)
+// {
+//     System.Console.Write(Math.Pow(count, 2) + " ");
+//     count ++;
+// }
 
-while (count <= N)
+for (int i = 1; i <= N; i++)
 {
-    System.Console.Write(Math.Pow(count, 2) + " ");
-    count ++;
+    System.Console.Write(Math.Pow(i, 2) + " ");
 }
+
+//когда известно, до какого числа цикл продолжается, лучше цикл For использовать
