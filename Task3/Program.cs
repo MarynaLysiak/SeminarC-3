41eb3b86ca96f41a0f6981503355b7a2d09e5bf5
Maryna <maryna.lysyak@gmail.com>
2023-02-06 16:32:23 +0300

    Задача на вычисление расстояния между точками матем функции

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+// Математические функции: Math.Pow(98, 2) - 98 возводим во вторую степень
+//  Math.Sqrt(25) - вычисление квадратного корня,
+// Округление числа Math.Round(a,3) - округлить число а до 3 точек после запятой
+// Формула для нахождения расстояния между 2 точками: ((х2-х1)ˆ2+(y2-y1)ˆ2)ˆ1/2 (корень квадратный из суммы)
+
+int InputNumber(string stroka)
+{
+    System.Console.Write(stroka);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int x1 = InputNumber("Введите координату х1: ");
+int y1 = InputNumber("Введите координату у1: ");
+int x2 = InputNumber("Введите координату х1: ");
+int y2 = InputNumber("Введите координату у1: ");
+
+double distance = Math.Round(Math.Sqrt(Math.Pow((x2-x1),2)+ Math.Pow((y2-y1),2)),3);
+System.Console.WriteLine(distance);
