382118ad8c121f537bdf75f19f8e1cc142540d80
Maryna <maryna.lysyak@gmail.com>
2023-02-09 12:38:29 +0300

    Задача на сумму всех чисел от 1 до А

---
+++ Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая берет на входе число, а выдает сумму чисел от 1 до А
+
+System.Console.WriteLine("Введите число А: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+for (int i = 1; i <= num ; i++)
+{
+    result+=i; //result = result + i
+}
+System.Console.WriteLine($" Сумма чисел от 1 до {num} равна {result}");
