8c858e6a6cb6ecc55c4c3639fab2bd52e59f8855
Maryna <maryna.lysyak@gmail.com>
2023-02-09 12:51:25 +0300

    Функции

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,11 +1,35 @@
 ﻿// Напишите программу, которая берет на входе число, а выдает сумму чисел от 1 до А
 
-System.Console.WriteLine("Введите число А: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int result = 0;
+// System.Console.WriteLine("Введите число А: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = 0;
 
-for (int i = 1; i <= num ; i++)
+// for (int i = 1; i <= num ; i++)
+// {
+//     result+=i; //result = result + i
+// }
+// System.Console.WriteLine($" Сумма чисел от 1 до {num} равна {result}");
+
+// Решение с использованием функций
+
+int GetNumber(string text)
 {
-    result+=i; //result = result + i
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
 }
-System.Console.WriteLine($" Сумма чисел от 1 до {num} равна {result}");
+
+int GetSumElements(int A)
+{
+    int result = 0;
+    for (int i = 1; i <= A; i++)
+    {
+        result += i; // result = result + i;
+    }
+
+    return result;
+}
+
+int num = GetNumber("Введите число А: ");
+int res = GetSumElements(num);
+
+System.Console.WriteLine($"Сумма чисел от 1 до {num} = {res}");
