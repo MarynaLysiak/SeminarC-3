3e76d24b208769ca1ae1396fb6900e1bdcb1b7d8
Maryna <maryna.lysyak@gmail.com>
2023-02-09 16:52:08 +0300

    Подсчет символов в введенном числе

---
+++ Task6/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+string? GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return System.Console.ReadLine();
+}
+
+string? abc = GetNumber("Введите число N: ");
+int length = abc!.Length;
+
+System.Console.WriteLine($"в числе {abc} - {length} цифр");
