ab4599f0bbb5a2d2a4971bdc5cd34604d7cc8d0f
Maryna <maryna.lysyak@gmail.com>
2023-02-16 16:04:50 +0300

    Семинар 6

---
+++ MetkaTriangle/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+metka:
+System.Console.WriteLine("Введите числа: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+if(array.Length!= 3)
+{
+System.Console.WriteLine("Массив введён некоректно ");
+goto metka;
+}
+
+if ((array[0] + array[1]) > array[2] && (array[1] + array[2]) > array[0] && (array[0] + array[2] > array[1]))
+{
+System.Console.WriteLine("Такой треугольник существует ");
+}
+else{
+System.Console.WriteLine("Такой треугольник не существует ");
+}
