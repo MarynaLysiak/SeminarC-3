b18fdcefd3c5d0506a4f975ba32a3a703269f08f
Maryna <maryna.lysyak@gmail.com>
2023-02-18 17:52:19 +0300

    Gjlcxtn gjkj;bntkmys[ xbctk d vfccbdПодсчет положиетльных чисел в массиве

---
+++ CountPositiveElements/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.WriteLine("Введите числа через пробел: ");
+// Разбивает строку на инт элементы, введенные через пробел
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse); 
+
+int CountPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+System.Console.WriteLine($"В массиве {CountPositive(arr)} положительных числа");
