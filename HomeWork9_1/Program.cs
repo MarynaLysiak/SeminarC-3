133f831d426e00534e3b0afe222a1a3c1d8f269a
Maryna <maryna.lysyak@gmail.com>
2023-03-01 17:23:35 +0300

    рекурссия сумма чисел в промежутке

---
+++ HomeWork9_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int NumM = GetNumbers("Введите число M начало промежутка");
+int NumN = GetNumbers("Введите число N конец промежутка");
+System.Console.WriteLine($"Сумма чисел от {NumM} до {NumN} = {GetSum(NumM, NumN)}");
+
+int GetNumbers(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+int GetSum(int m, int n)
+{
+    if (n == m) 
+    {
+        return m;
+    }  
+    return GetSum(m, n-1) + n;
+}
+
+
+
